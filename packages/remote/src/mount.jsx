313969274ecceb98d2bae861b43ad39d55bf2879
Darcy LF <ehnevault@gmail.com>
2021-04-09 16:05:58 +1000

    move dev server rendering out

---
+++ packages/remote/src/mount.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Main from './index';
+
+const mountNode = document.getElementById('app');
+ReactDOM.render(<Main />, mountNode);
