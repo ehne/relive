4ffa3a74d31e02fa599bb3151eea9d4f3a9cbfeb
Darcy LF <ehnevault@gmail.com>
2021-04-11 18:02:57 +1000

    move remote styles back into the remote package

--- packages/remote/src/index.jsx
+++ packages/remote/src/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Card from './components/Card';
+import Styles from './styles';
 
 const Main = () => (
-  <main>
-    <Card title="cool" callbackFn={() => {}} />
-  </main>
+  <>
+    <Styles />
+    <main>
+      <Card title="cool" callbackFn={() => {}} />
+    </main>
+  </>
 );
 
 export default Main;
